cc0d08e9406b520f6855951a2d4f8e0d8ee3b18e
Likhith08105 <likhithpaidimarri0815@gmail.com>
2026-05-05 11:22:37 +0530

    final fixes

--- backend/models/User.js
+++ backend/models/User.js
@@ -48,7 +48,7 @@ userSchema.pre('save', async function (next) {
   }
 });
 
-/
+
 userSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
